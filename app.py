b004cdea9399ea34f7f206a83f2c42026a992c71
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-15 23:35:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> 
+
 @app.route("/")
 def index():
   return "Olá, mundo! Esse é meu site. (Lucas Duarte)"
 
 @app.route("/sobre")
 def sobre():
-  return "Um site muito paid'égua! Veja só!"
+  return "Um site muito paid'égua! Vejam só!"
 
 @app.route("/contato")
 def contato():
