b204024dbf246acb33316fd3cab687490557ee23
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-15 23:42:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
 menu = """
-<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> 
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
 
 @app.route("/")
 def index():
