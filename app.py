beef7e3d8cfa0b96f00eeb6c5e5c9aa4527209f2
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-20 20:41:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from tchan import ChannelScraper
 
+
 app = Flask(__name__)
+
 
 def ultimas_promocoes():
   scraper = ChannelScraper()
@@ -14,6 +16,7 @@ def ultimas_promocoes():
     if contador == 10:
       return resultado
 
+    
 menu = """
 <a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
@@ -21,15 +24,16 @@ menu = """
 
 @app.route("/")
 def index():
-  return "Olá, mundo! Esse é meu site. (Lucas Duarte)"
+  return menu + "Olá, mundo! Esse é meu site. (Álvaro Justen)"
 
 @app.route("/sobre")
 def sobre():
-  return "Um site muito paid'égua! Vejam só!"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "lucasduartematos@gmail.com"
+  return menu + "Aqui vai o conteúdo da página Contato"
+
 
 @app.route("/promocoes")
 def promocoes():
@@ -40,4 +44,22 @@ def promocoes():
   """
   for promocao in ultimas_promocoes():
     conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
   return conteudo + "</ul>"
