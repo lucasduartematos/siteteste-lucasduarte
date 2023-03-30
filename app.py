16cc3ce6e550d331788bcaa784ae8880f9af6e87
Lucas Duarte <114267308+lucasduartematos@users.noreply.github.com>
2023-03-30 10:30:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import os
+
+import gspread
+import requests
+from flask import Flask, request
+from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
 
 
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
+TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM")
+sheet = planilha.worksheet("Sheet1")
 app = Flask(__name__)
 
 
@@ -24,15 +38,15 @@ menu = """
 
 @app.route("/")
 def index():
-  return menu + "Olá, mundo! Esse é meu site. (Lucas Duarte)"
+  return menu + "Olá, mundo! Esse é meu site. (Álvaro Justen)"
 
 @app.route("/sobre")
 def sobre():
-  return menu + "O conteúdo é Pai D'égua!"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return menu + "Este é o contato: lucasduartematos@gmail.com"
+  return menu + "Aqui vai o conteúdo da página Contato"
 
 
 @app.route("/promocoes")
@@ -63,3 +77,25 @@ def promocoes2():
     if contador == 10:
       break
   return conteudo + "</ul>"
+
+@app.route("/dedoduro")
+def dedoduro():
+  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
+  resposta = requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
+  return f"Mensagem enviada. Resposta ({resposta.status_code}): {resposta.text}"
+
+
+@app.route("/dedoduro2")
+def dedoduro2():
+  sheet.append_row(["Álvaro", "Justen", "a partir do Flask"])
+  return "Planilha escrita!"
+
+
+@app.route("/telegram-bot", methods=["POST"])
+def telegram_bot():
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"]
+  nova_mensagem = {"chat_id": chat_id, "text": message}
+  requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  return "ok"
